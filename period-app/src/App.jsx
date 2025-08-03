e5d7a0371dcf8174e6b2579c42412a90cab2cb42
Roman Joseph Gallardo <romanjosephgallardo01@gmail.com>
2025-08-03 10:18:07 +0800

    feat: add initial global styles

--- period-app/src/App.jsx
+++ period-app/src/App.jsx
@@ -4,6 +4,8 @@ import DotControls from './components/DotControls/DotControls'
 import Header from './components/Header/Header'
 import ImageUpload from './components/ImageUpload/ImageUpload'
 
+import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
